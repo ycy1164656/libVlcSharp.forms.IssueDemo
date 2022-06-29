999d5861eecc9fe1e1ef3a82ce3cebfe764889d4
ycy <ycy1164656@gmail.com>
2022-06-29 16:49:31 +0800

    issue

---
+++ MediaElement/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace MediaElement
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnAppearing(object sender, System.EventArgs e)
+        {
+            base.OnAppearing();
+            ((MainViewModel)BindingContext).OnAppearing();
+        }
+
+        void ContentPage_Disappearing(System.Object sender, System.EventArgs e)
+        {
+            base.OnDisappearing();
+            ((MainViewModel)BindingContext).Disappearing();
+
+        }
+    }
+}
---
+++ MediaElement/MainViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.ComponentModel;
+using LibVLCSharp.Shared;
+
+namespace MediaElement
+{
+    /// <summary>
+    /// Represents the main viewmodel.
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Property changed event
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+
+        private LibVLC _libVLC;
+        /// <summary>
+        /// Gets the <see cref="LibVLCSharp.Shared.LibVLC"/> instance.
+        /// </summary>
+        public LibVLC LibVLC
+        {
+            get => _libVLC;
+            private set => Set(nameof(LibVLC), ref _libVLC, value);
+        }
+
+        private MediaPlayer _mediaPlayer;
+        /// <summary>
+        /// Gets the <see cref="LibVLCSharp.Shared.MediaPlayer"/> instance.
+        /// </summary>
+        public MediaPlayer MediaPlayer
+        {
+            get => _mediaPlayer;
+            private set => Set(nameof(MediaPlayer), ref _mediaPlayer, value);
+        }
+
+        /// <summary>
+        /// Initialize LibVLC and playback when page appears
+        /// </summary>
+        public void OnAppearing()
+        {
+            Core.Initialize();
+
+            LibVLC = new LibVLC();
+
+            var media = new Media(LibVLC,
+                new Uri("http://streams.videolan.org/streams/mkv/multiple_tracks.mkv"));
+            
+
+            MediaPlayer = new MediaPlayer(media) { EnableHardwareDecoding = true };
+            media.Dispose();
+
+
+            /*
+             
+             
+             
+             
+             no matter what event will cause the issue
+
+
+             
+             
+             
+             */
+
+            MediaPlayer.TimeChanged += MediaPlayer_TimeChanged;
+            MediaPlayer.PositionChanged += MediaPlayer_PositionChanged;
+
+
+            /*
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             *
+             */
+
+
+            MediaPlayer.Play();
+        }
+
+        private void MediaPlayer_PositionChanged(object sender, MediaPlayerPositionChangedEventArgs e)
+        {
+             
+        }
+
+        public void Disappearing()
+        {
+            MediaPlayer.Dispose();
+            LibVLC.Dispose();
+        }
+
+        private void MediaPlayer_TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
+        {
+             
+        }
+
+        private void Set<T>(string propertyName, ref T field, T value)
+        {
+            if (field == null && value != null || field != null && !field.Equals(value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
